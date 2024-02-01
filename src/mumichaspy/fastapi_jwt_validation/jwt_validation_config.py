f3dac00b9ea7aca81907385a5dac5529d545719f
Alain Perez <aperez@mondragon.edu>
2024-02-01 17:51:13 +0100

    FIRST JWT VALIDATION TESTS PASSED

---
+++ src/mumichaspy/fastapi_jwt_validation/jwt_validation_config.py
@@ -0,0 +1,41 @@
+import os
+import logging
+
+
+logger = logging.getLogger(__name__)
+
+
+class Config:
+    public_key = None
+    public_key_url = os.getenv(
+        "PUBLIC_KEY_URL", "https://auth.alainperez.duckdns.org/public_key"
+    )
+    jwt_issuer = os.getenv("JWT_ISSUER", "aperez")
+    jwt_algorithm = os.getenv("JWT_ALGORITHM", "RS256")
+
+    def __init__(self):
+        self.update_public_key()
+
+    def update_public_key(self):
+        import httpx
+
+        try:
+            with httpx.Client() as client:
+                response = client.get(self.public_key_url)
+                if response.status_code == 200:
+                    self.public_key = response.text
+                    with open("public_key.pem", "w") as f:
+                        f.write(self.public_key)
+
+        except Exception as e:
+            logger.error(
+                "Last public key will be loaded from file. Error loading public key: "
+                + str(e)
+            )
+
+            # load last public key from file
+            with open("public_key.pem", "r") as f:
+                self.public_key = f.read()
+
+
+config = Config()
---
+++ tests/fastapi_jwt_validation/test_jwt_validation_helpers.py
@@ -0,0 +1,62 @@
+import time
+from fastapi import HTTPException, status
+import jwt
+import pytest
+from mumichaspy.fastapi_jwt_validation.jwt_validation_config import config
+
+from mumichaspy.fastapi_jwt_validation.jwt_validation_helperes import (
+    validate_and_decode_token,
+)
+from tests.fastapi_jwt_validation.helpers import TESTING_PUBLIC_KEY, TESTING_PRIVATE_KEY
+
+config.public_key = TESTING_PUBLIC_KEY
+
+
+def test_validate_and_decode_token():
+    # Arrange
+    expected_decoded_token = {
+        "user_id": "123",
+        "iss": config.jwt_issuer,
+        "exp": int(time.time()) + 60,
+    }
+    encoded_token = jwt.encode(
+        expected_decoded_token, TESTING_PRIVATE_KEY, algorithm=config.jwt_algorithm
+    )
+
+    # Act
+    result = validate_and_decode_token(
+        encoded_token,
+        TESTING_PUBLIC_KEY,
+        expected_decoded_token["iss"],
+        [config.jwt_algorithm],
+    )
+
+    # Assert
+    assert result == expected_decoded_token
+
+
+def test_validate_and_decode_token_exp_error():
+    # Arrange
+    expected_decoded_token = {
+        "user_id": "123",
+        "iss": config.jwt_issuer,
+        "exp": int(time.time()) - 60,
+    }
+    encoded_token = jwt.encode(
+        expected_decoded_token, TESTING_PRIVATE_KEY, algorithm=config.jwt_algorithm
+    )
+
+    # Act
+    with pytest.raises(HTTPException, match="403: Could not decode JWT") as exc:
+        validate_and_decode_token(
+            encoded_token,
+            TESTING_PUBLIC_KEY,
+            expected_decoded_token["iss"],
+            [config.jwt_algorithm],
+        )
+
+        assert exc.status_code == status.HTTP_403_FORBIDDEN
+
+
+# @Todo: Add more tests for validate_and_decode_token
+# @ToDo: Add more tests for JWTBearer and JWTBearerAdmin
