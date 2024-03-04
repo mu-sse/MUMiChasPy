3d5504613699e659614bfcd09e1941c0b98fda49
Alain Perez <aperez@mondragon.edu>
2024-03-04 18:20:29 +0100

    JWT CONFIG FUNCTION RESPONSABILITIES REDUCED, PUBLIC KEY CAN BE SET NOW, DEFAULT PUBLIC KEY URL IS ""

--- src/mumichaspy/fastapi_jwt_chassis/config.py
+++ src/mumichaspy/fastapi_jwt_chassis/config.py
@@ -5,12 +5,16 @@ import logging
 logger = logging.getLogger(__name__)
 
 
-def get_public_key_from_url(public_key_url: str, file_path: str):
+def get_public_key_from_url(public_key_url: str):
     """Upload public key from URL."""
 
     import httpx
 
     public_key = None
+
+    if public_key_url is None or public_key_url == "":
+        logger.warning("No public key URL provided")
+        return None
 
     try:
         with httpx.Client() as client:
@@ -20,9 +24,6 @@ def get_public_key_from_url(public_key_url: str, file_path: str):
                 raise Exception(f"Status code: {response.status_code}")
 
             public_key = response.text
-
-            with open(file_path, "w") as f:
-                f.write(public_key)
 
     except Exception as e:
         logger.warning("Could not load public key from URL: " + str(e))
@@ -46,9 +47,34 @@ def get_public_key_from_file(file_path: str):
     return public_key
 
 
+def write_public_key_to_file(public_key: str, file_path: str):
+    """Write public key to file."""
+    try:
+        with open(file_path, "w") as f:
+            f.write(public_key)
+    except Exception as e:
+        logger.warning("Could not write public key to file: " + str(e))
+
+
+def get_public_key(
+    public_key: str = None,
+    public_key_url: str = None,
+    public_key_file_path: str = None,
+):
+    """Get public key."""
+
+    if public_key is None or public_key == "":
+        public_key = get_public_key_from_url(public_key_url)
+
+    if public_key is None or public_key == "":
+        public_key = get_public_key_from_file(public_key_file_path)
+
+    return public_key
+
+
 class Config:
     public_key = None
-    public_key_url = os.getenv("PUBLIC_KEY_URL", "https://auth.example.com/pk")
+    public_key_url = os.getenv("PUBLIC_KEY_URL", "")
     jwt_issuer = os.getenv("JWT_ISSUER", "mu-sse")
     jwt_algorithm = os.getenv("JWT_ALGORITHM", "RS256")
     public_key_file_path = os.getenv("PUBLIC_KEY_FILE_PATH", "public_key.pem")
@@ -56,21 +82,20 @@ class Config:
     def __init__(self):
         self.update_public_key()
 
-    def update_public_key(self):
+    def update_public_key(self, public_key=None, public_key_file_path=None):
         """Update public key."""
-        if self.public_key_url is not None and self.public_key_url != "":
-            self.public_key = get_public_key_from_url(
-                self.public_key_url, self.public_key_file_path
-            )
+        if public_key_file_path is not None:
+            self.public_key_file_path = public_key_file_path
 
-        if (
-            self.public_key is None
-            and self.public_key_file_path is not None
-            and self.public_key_file_path != ""
-        ):
-            self.public_key = get_public_key_from_file(self.public_key_file_path)
+        public_key = get_public_key(
+            public_key, self.public_key_url, self.public_key_file_path
+        )
 
-        if self.public_key is None:
+        if public_key is not None and public_key != "":
+            self.public_key = public_key
+            write_public_key_to_file(self.public_key, self.public_key_file_path)
+
+        else:
             logger.error("No public key available")
 
 
--- tests/fastapi_jwt_chassis/test_config.py
+++ tests/fastapi_jwt_chassis/test_config.py
@@ -1,67 +1,65 @@
 import os
-import random
-import string
+import uuid
 from unittest import TestCase, mock
 import tempfile
 from mumichaspy.fastapi_jwt_chassis.config import (
     Config,
     get_public_key_from_url,
     get_public_key_from_file,
+    write_public_key_to_file,
+    get_public_key,
 )
 from mumichaspy.fastapi_jwt_chassis.mocks import TESTING_PUBLIC_KEY
 
 
+# get_public_key_from_url #########################################################################
 @mock.patch("httpx.Client.get")
 def test_get_public_key_from_url_ok(mock_get):
     # Arrange
-    with tempfile.NamedTemporaryFile(delete=True) as temp:
-        public_key_file_name = temp.name
+    mock_response = mock.Mock()
+    mock_response.status_code = 200
+    mock_response.text = TESTING_PUBLIC_KEY
+    mock_get.return_value = mock_response
+    url = "https://example.com/public_key"
 
-        mock_response = mock.Mock()
-        mock_response.status_code = 200
-        mock_response.text = TESTING_PUBLIC_KEY
+    # Act
+    public_key = get_public_key_from_url(url)
 
-        mock_get.return_value = mock_response
-
-        if os.path.isfile(public_key_file_name):
-            os.remove(public_key_file_name)
-
-        # Act
-        public_key = get_public_key_from_url("", public_key_file_name)
-
-        # Assert
-        assert public_key == TESTING_PUBLIC_KEY
-        assert os.path.isfile(public_key_file_name)
-        with open(public_key_file_name, "r") as f:
-            assert f.read() == TESTING_PUBLIC_KEY
+    # Assert
+    mock_get.assert_called_once_with(url)
+    assert public_key == TESTING_PUBLIC_KEY
 
 
 @mock.patch("httpx.Client.get")
 def test_get_public_key_from_url_error(mock_get):
     # Arrange
-    public_key_file_name = (
-        "".join(
-            random.choice(string.ascii_uppercase + string.digits) for _ in range(12)
-        )
-        + "_public_key.pem"
-    )
     mock_response = mock.Mock()
     mock_response.status_code = 404
-
     mock_get.return_value = mock_response
-    if os.path.isfile(public_key_file_name):
-        os.remove(public_key_file_name)
+    url = "https://example.com/public_key"
 
     # Act
-    public_key = get_public_key_from_url(
-        public_key_url="", file_path=public_key_file_name
-    )
+    public_key = get_public_key_from_url(url)
 
     # Assert
+    mock_get.assert_called_once_with(url)
     assert public_key is None
-    assert not os.path.isfile(public_key_file_name)
 
 
+@mock.patch("httpx.Client.get")
+def test_get_public_key_from_url_no_url(mock_get):
+    # Arrange
+    url = ""
+
+    # Act
+    public_key = get_public_key_from_url(public_key_url=url)
+
+    # Assert
+    mock_get.assert_not_called()
+    assert public_key is None
+
+
+# get_public_key_from_file ########################################################################
 def test_update_public_key_from_file_ok():
     # Arrange
     with tempfile.NamedTemporaryFile(delete=False) as temp:
@@ -77,59 +75,208 @@ def test_update_public_key_from_file_ok():
     os.remove(public_key_file_name)
 
 
-def test_update_public_key_from_file_error():
+@mock.patch("mumichaspy.fastapi_jwt_chassis.config.logger.warning")
+def test_update_public_key_from_file_no_file(logger_warning_mock):
     # Arrange
-    with tempfile.NamedTemporaryFile(delete=True) as temp:
-        public_key_file_name = temp.name
+    filename = f"{uuid.uuid4().hex}.pem"
 
     # Act
-    public_key = get_public_key_from_file(file_path=public_key_file_name)
+    public_key = get_public_key_from_file(file_path=filename)
 
     # Assert
+    logger_warning_mock.assert_called_once_with(
+        "Could not load public key from file: Public key file not found"
+    )
     assert public_key is None
 
 
+# write_public_key_to_file ########################################################################
+def test_write_public_key_to_file_ok():
+    # Arrange
+    with tempfile.NamedTemporaryFile(delete=False) as temp:
+        public_key_file_name = temp.name
+
+    # Act
+    write_public_key_to_file(
+        public_key=TESTING_PUBLIC_KEY, file_path=public_key_file_name
+    )
+
+    # Assert
+    with open(public_key_file_name, "r") as f:
+        public_key = f.read()
+    assert public_key == TESTING_PUBLIC_KEY
+    os.remove(public_key_file_name)
+
+
+# get_public_key ##################################################################################
+@mock.patch("mumichaspy.fastapi_jwt_chassis.config.get_public_key_from_file")
+@mock.patch("mumichaspy.fastapi_jwt_chassis.config.get_public_key_from_url")
+def test_get_public_key_ok_public_key(mock_from_url, mock_from_file):
+    # Arrange
+    public_key = TESTING_PUBLIC_KEY
+    public_key_url = "https://example.com/public_key"
+    public_key_file_path = f"{uuid.uuid4().hex}.pem"
+
+    # Act
+    result = get_public_key(
+        public_key=public_key,
+        public_key_url=public_key_url,
+        public_key_file_path=public_key_file_path,
+    )
+
+    # Assert
+    mock_from_url.assert_not_called()
+    mock_from_file.assert_not_called()
+    assert result == TESTING_PUBLIC_KEY
+
+
+@mock.patch("mumichaspy.fastapi_jwt_chassis.config.get_public_key_from_file")
+@mock.patch("mumichaspy.fastapi_jwt_chassis.config.get_public_key_from_url")
+def test_get_public_key_ok_url(mock_from_url, mock_from_file):
+    # Arrange
+    public_key = ""
+    public_key_url = "https://example.com/public_key"
+    public_key_file_path = f"{uuid.uuid4().hex}.pem"
+    mock_from_url.return_value = TESTING_PUBLIC_KEY
+
+    # Act
+    result = get_public_key(
+        public_key=public_key,
+        public_key_url=public_key_url,
+        public_key_file_path=public_key_file_path,
+    )
+
+    # Assert
+    mock_from_url.assert_called_once_with(public_key_url)
+    mock_from_file.assert_not_called()
+    assert result == TESTING_PUBLIC_KEY
+
+
+@mock.patch("mumichaspy.fastapi_jwt_chassis.config.get_public_key_from_file")
+@mock.patch("mumichaspy.fastapi_jwt_chassis.config.get_public_key_from_url")
+def test_get_public_key_ok_public_file(mock_from_url, mock_from_file):
+    # Arrange
+    public_key = ""
+    public_key_url = "https://example.com/public_key"
+    public_key_file_path = f"{uuid.uuid4().hex}.pem"
+    mock_from_url.return_value = None
+    mock_from_file.return_value = TESTING_PUBLIC_KEY
+
+    # Act
+    result = get_public_key(
+        public_key=public_key,
+        public_key_url=public_key_url,
+        public_key_file_path=public_key_file_path,
+    )
+
+    # Assert
+    mock_from_url.assert_called_once_with(public_key_url)
+    mock_from_file.assert_called_once_with(public_key_file_path)
+    assert result == TESTING_PUBLIC_KEY
+
+
 class TestConfig(TestCase):
+    url = "https://test.com/public_key"
+    issuer = "test-issuer"
+    algorithm = "RS256"
+    file_path = f"{uuid.uuid4().hex}.pem"
+
+    # @mock.patch("os.getenv")
     def setUp(self):
+        # mock_getenv.side_effect = lambda x, default=None: {
+        #     "PUBLIC_KEY_URL": self.url,
+        #     "JWT_ISSUER": self.issuer,
+        #     "JWT_ALGORITHM": self.algorithm,
+        #     "PUBLIC_KEY_FILE_PATH": self.file_path,
+        # }.get(x, default)
+
         self.config = Config()
 
-    @mock.patch("mumichaspy.fastapi_jwt_chassis.config.get_public_key_from_url")
-    def test_update_public_key_url_ok(self, mock_get_public_key_from_url):
+        # assert self.config.public_key is None
+        # assert self.config.public_key_url == url
+        # assert self.config.jwt_issuer == self.issuer
+        # assert self.config.jwt_algorithm == self.algorithm
+        # assert self.config.public_key_file_path == self.file_path
+
+    @mock.patch("mumichaspy.fastapi_jwt_chassis.config.write_public_key_to_file")
+    @mock.patch("mumichaspy.fastapi_jwt_chassis.config.get_public_key")
+    def test_update_public_key_ok(self, mock_get_public_key, mock_write_public_key):
         # Arrange
-        mock_get_public_key_from_url.return_value = TESTING_PUBLIC_KEY
+        self.config.public_key = None
+        self.config.public_key_url = self.url
+        self.config.public_key_file_path = self.file_path
+        mock_get_public_key.return_value = TESTING_PUBLIC_KEY
 
         # Act
         self.config.update_public_key()
 
         # Assert
+        mock_get_public_key.assert_called_once_with(None, self.url, self.file_path)
+        mock_write_public_key.assert_called_once_with(
+            TESTING_PUBLIC_KEY, self.file_path
+        )
         assert self.config.public_key == TESTING_PUBLIC_KEY
 
-    @mock.patch("mumichaspy.fastapi_jwt_chassis.config.get_public_key_from_file")
-    @mock.patch("mumichaspy.fastapi_jwt_chassis.config.get_public_key_from_url")
-    def test_update_public_key_no_url_file_ok(
-        self, mock_get_public_key_from_url, mock_get_public_key_from_file
+    @mock.patch("mumichaspy.fastapi_jwt_chassis.config.write_public_key_to_file")
+    @mock.patch("mumichaspy.fastapi_jwt_chassis.config.get_public_key")
+    def test_update_public_key_ok_with_public_key(
+        self, mock_get_public_key, mock_write_public_key
     ):
         # Arrange
-        mock_get_public_key_from_url.return_value = None
-        mock_get_public_key_from_file.return_value = TESTING_PUBLIC_KEY
+        self.config.public_key_url = self.url
+        self.config.public_key_file_path = self.file_path
+        mock_get_public_key.return_value = TESTING_PUBLIC_KEY
 
         # Act
-        self.config.update_public_key()
+        self.config.update_public_key(public_key=TESTING_PUBLIC_KEY)
 
         # Assert
+        mock_get_public_key.assert_called_once_with(
+            TESTING_PUBLIC_KEY, self.url, self.file_path
+        )
+        mock_write_public_key.assert_called_once_with(
+            TESTING_PUBLIC_KEY, self.file_path
+        )
         assert self.config.public_key == TESTING_PUBLIC_KEY
 
-    @mock.patch("mumichaspy.fastapi_jwt_chassis.config.get_public_key_from_file")
-    @mock.patch("mumichaspy.fastapi_jwt_chassis.config.get_public_key_from_url")
-    def test_update_public_key_no_url_file_error(
-        self, mock_get_public_key_from_url, mock_get_public_key_from_file
+    @mock.patch("mumichaspy.fastapi_jwt_chassis.config.write_public_key_to_file")
+    @mock.patch("mumichaspy.fastapi_jwt_chassis.config.get_public_key")
+    def test_update_public_key_ok_with_file_path(
+        self, mock_get_public_key, mock_write_public_key
     ):
         # Arrange
-        mock_get_public_key_from_url.return_value = None
-        mock_get_public_key_from_file.return_value = None
+        self.config.public_key_url = self.url
+        self.config.public_key_file_path = "test.pem"
+        mock_get_public_key.return_value = TESTING_PUBLIC_KEY
+
+        # Act
+        self.config.update_public_key(
+            public_key=TESTING_PUBLIC_KEY, public_key_file_path=self.file_path
+        )
+
+        # Assert
+        mock_get_public_key.assert_called_once_with(
+            TESTING_PUBLIC_KEY, self.url, self.file_path
+        )
+        mock_write_public_key.assert_called_once_with(
+            TESTING_PUBLIC_KEY, self.file_path
+        )
+        assert self.config.public_key == TESTING_PUBLIC_KEY
+        assert self.config.public_key_file_path == self.file_path
+
+    @mock.patch("mumichaspy.fastapi_jwt_chassis.config.write_public_key_to_file")
+    @mock.patch("mumichaspy.fastapi_jwt_chassis.config.get_public_key")
+    def test_update_public_key_error(self, mock_get_public_key, mock_write_public_key):
+        # Arrange
+        self.config.public_key = None
+        self.config.public_key_url = self.url
+        self.config.public_key_file_path = self.file_path
+        mock_get_public_key.return_value = None
 
         # Act
         self.config.update_public_key()
 
         # Assert
+        mock_get_public_key.assert_called_once_with(None, self.url, self.file_path)
+        mock_write_public_key.assert_not_called()
         assert self.config.public_key is None
